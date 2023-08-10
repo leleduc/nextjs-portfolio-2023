f92a19415130de4d812fde3812682774f0355447
leleduc <leleduc@gmail.com>
2023-08-10 11:27:41 +0700

    Content init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image';
+import { Contact, Hero, Projects, Skills } from '@/components';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Main content
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
     </main>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
 import Header from './Header';
 import Footer from './Footer';
+import Hero from './Hero';
+import Skills from './Skills';
+import Projects from './Projects';
+import Contact from './Contact';
 
-export { Header, Footer };
+export { Header, Footer, Hero, Skills, Projects, Contact };
